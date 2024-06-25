98bf2384e11d5966b3d7eb92aecfd72f0d556d92
pokycookie <0cookieboy0@gmail.com>
2024-06-25 23:06:41 +0900

    myBatis

---
+++ itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -0,0 +1,23 @@
+package hello.itemservice.repository.mybatis;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface ItemMapper {
+
+    void save(Item item);
+
+    // 파라미터가 2개 이상인 경우 @Param 어노테이션 사용
+    void update(@Param("id") Long id, @Param("updateParam")ItemUpdateDto updateParam);
+
+    Optional<Item> findById(Long id);
+
+    List<Item> findAll(ItemSearchCond itemSearch);
+}
